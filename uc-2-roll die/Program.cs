cc75cca7e88377ec7525b608de9b7d8da64a32ea
senpooja <poojasen744055@gmail.com>
2022-08-03 19:04:54 +0530

    wellcome uc-2 file

---
+++ uc-2-roll die/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("wellcome in snake .");
+int position = 0;
+Random random = new Random();
+int rolldie = random.Next(1, 7);
+Console.WriteLine("player 1 position is  " + position );
+Console.WriteLine("roll die value " + rolldie);
