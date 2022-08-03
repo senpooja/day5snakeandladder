d7d5b4f5d778af495597a07311ceab92737d10ce
senpooja <poojasen744055@gmail.com>
2022-08-03 19:07:26 +0530

    wellcome uc-3 file

---
+++ uc-3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Welcome to snake ladder game");
+
+{
+    Random Dice = new Random();
+    int dice = Dice.Next(1, 7);
+
+    //no play condition
+    if (dice == 5)
+    {
+        Console.WriteLine("player on ledder");
+
+    }
+    else if (dice == 4)
+    {
+        Console.WriteLine("payer on snake");
+
+    }
+    else
+    {
+        Console.WriteLine("no plays");
+    }
+}
